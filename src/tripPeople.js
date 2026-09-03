283f01e157e907d0dda7b50d4c967471b90ee9da
nadylime <danmagstadt@gmail.com>
2026-09-03 11:43:21 -0400

    Update trip roster and arrival flight details

--- src/tripPeople.js
+++ src/tripPeople.js
@@ -1,2 +1,4 @@
-export const PEOPLE=['Dan','Emily','Lyssie','Ashton','Alec','Addison'];
+export const TRAVELERS=['Dan','Emily','Lyssie','Ashton','Alec'];
+export const REMOTE_PARTICIPANTS=['Alexis'];
+export const PEOPLE=[...TRAVELERS,...REMOTE_PARTICIPANTS];
 export const OVERVIEW=[['Thu 3','Arrive Denver'],['Fri 4','Adventure and Salida'],['Sat 5','Saturday adventures'],['Sat 5','Saturday evening wedding'],['Sun 6','Adventure or chill day'],['Mon 7','Adventure or slow day'],['Tue 8','Fly home']];
