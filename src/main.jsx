2775cd989590dac6b2cc13358edd1082ddc9975e
nadylime <danmagstadt@gmail.com>
2026-09-05 09:24:17 -0400

    Enable offline use and automatic updates

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
+
+if('serviceWorker' in navigator){
+  const hadController=Boolean(navigator.serviceWorker.controller);
+  let reloading=false;
+  window.addEventListener('load',()=>{
+    navigator.serviceWorker.register('/sw.js',{updateViaCache:'none'})
+      .then(registration=>registration.update())
+      .catch(()=>{});
+  });
+  navigator.serviceWorker.addEventListener('controllerchange',()=>{
+    if(hadController&&!reloading&&!window.__APP_RELOADING__){
+      reloading=true;
+      window.__APP_RELOADING__=true;
+      window.location.reload();
+    }
+  });
+}
